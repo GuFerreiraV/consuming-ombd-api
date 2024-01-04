0a746e67c4df90fa75bfa6b649817615c6e1ad62
Gustavo Ferreira <100845421+Ferre1ra1@users.noreply.github.com>
2024-01-04 17:56:55 -0300

    Update Endereco.java

--- src/br/com/project/screenmatch/estudosExtras/DesafioApi/Endereco.java
+++ src/br/com/project/screenmatch/estudosExtras/DesafioApi/Endereco.java
@@ -1,4 +1,4 @@
-package br.com.alura.screenmatch.estudosExtras.DesafioApi;
+package br.com.project.screenmatch.estudosExtras.DesafioApi;
 
 public record Endereco(String cep, String logradouro, String bairro, String uf) {
 }
